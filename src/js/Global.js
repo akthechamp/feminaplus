1cc7b93ed4289f34fc6e91d5846bf3a9b2317c1b
WEB DEV <deepanshu4788@gmail.com>
2025-11-17 18:23:23 +0530

    Adjusted the header proper , Aligned the banner proper ,  rating section proper and  header, banner, and rating sections are fully responsive.

---
+++ src/js/Global.js
@@ -0,0 +1,32 @@
+import $ from "jquery";
+
+$(document).ready(function () {
+  $(".menu-click").click(function () {
+    $(".mobile-menu").slideDown(300);
+
+    $(this).hide();
+    $(".menu-close").show();
+
+    $("header").addClass("bg-white text-black h-20vh height-add");
+    $("header a, header p, header button")
+      .addClass("h-20vh")
+      .removeClass("text-white");
+    $("body").addClass("overflow-hidden");
+    $(".menu-bar").removeClass("hidden").addClass("!flex");
+  });
+
+  $(".menu-close").click(function () {
+    $(".mobile-menu").slideUp(300);
+    $(this).hide();
+    $(".menu-click").show();
+
+    $("header").removeClass("bg-white text-black h-20vh height-add");
+    $("header a, header p, header button")
+      .removeClass("h-20vh text-black")
+      .addClass("text-white");
+
+    $("body").removeClass("overflow-hidden");
+
+    $(".menu-bar").removeClass("!flex").addClass("hidden");
+  });
+});
