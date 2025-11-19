300ad8f06e0c65a309352298df92c2a0b42d4a66
wft-dev <dev@myweb-development.com>
2025-11-19 18:43:50 +0530

    Next Gen Health section ,  advertisment section , logo section compelte , Location section in progress

--- src/js/Global.js
+++ src/js/Global.js
@@ -1,34 +1,16 @@
 import $ from "jquery";
 
 $(document).ready(function () {
-  $(".menu-click").click(function () {
-    $(".mobile-menu").slideDown(300);
-
-    $(this).hide();
-    $(".menu-close").show();
-
-    $("header").addClass("bg-white text-black h-40vh height-add");
-    $("header a, header p, header button")
-      .addClass("h-20vh")
-      .removeClass("text-white");
+  $(".menu-click").on("click", function () {
+    $(".navigation").css("width", "100%");
     $("body").addClass("overflow-hidden");
-    $(".book-btn").hide();
-    $(".main-navbar").hide();
-    $(".menu-bar").removeClass("hidden").addClass("!flex");
   });
 
-  $(".menu-close").click(function () {
-    $(".mobile-menu").slideUp(300);
-    $(this).hide();
-    $(".menu-click").show();
-
-    $("header").removeClass("bg-white text-black h-20vh height-add");
-    $("header a, header p, header button")
-      .removeClass("h-20vh text-black")
-      .addClass("text-white");
-
+  $(".menu-close").on("click", function () {
+    $(".navigation").css("width", "0%");
     $("body").removeClass("overflow-hidden");
-
-    $(".menu-bar").removeClass("!flex").addClass("hidden");
+  });
+  $(".slider").on("beforeChange", function () {
+    $(".slick-slide::after").css({ width: 0, opacity: 0 });
   });
 });
